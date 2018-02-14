f497842d3f28288bcf32f08a0bbcd88df8510997
Thomas Schulte <thomas@cupracer.de>
2018-02-14 06:42:21 +0100

    use random string for Roundcube's DES key

--- roundcube/image/target/start.sh
+++ roundcube/image/target/start.sh
@@ -10,9 +10,11 @@ if [ "${MYSQL_PASSWORD}x" == "x" ]; then
 	exit 1
 fi
 
+DES_KEY="$(/usr/bin/pwgen -1cay 24)"
 
 sed -i "s/.*config.*db_dsnw.*/\$config\[\'db_dsnw\'\] = \'mysql\:\/\/app\:${MYSQL_PASSWORD//\//\\/}@roundcube-mysql\/app\'\;/" /etc/roundcubemail/config.inc.php
 sed -i "s/.*config.*smtp_server.*/\$config\[\'smtp_server\'\] = \'tls:\/\/${MYHOSTNAME//\//\\/}\'\;/" /etc/roundcubemail/config.inc.php
 sed -i "s/.*config.*default_host.*/\$config\[\'default_host\'\] = \'tls:\/\/${MYHOSTNAME//\//\\/}\'\;/" /etc/roundcubemail/config.inc.php
+sed -i "s/.*config.*des_key.*/\$config\[\'des_key\'\] = \'${DES_KEY//\//\\/}\'\;/" /etc/roundcubemail/config.inc.php
 
 /usr/sbin/start_apache2 -DFOREGROUND -k start
