9c40e16d83064abf36396b87991d72711cd4ee4f
Thomas Schulte <thomas@cupracer.de>
2018-07-12 16:55:21 +0200

    config moved in openSUSE Leap 15.0

--- postfixadmin/image/target/start.sh
+++ postfixadmin/image/target/start.sh
@@ -15,15 +15,15 @@ if [ "${SETUP_PASSWORD}x" == "x" ]; then
 	exit 1
 fi
 
-sed -i "s/MYHOSTNAME/${MYHOSTNAME}/g" /srv/www/htdocs/postfixadmin/config.local.php
-sed -i "s/MYSQL_PASSWORD/${MYSQL_PASSWORD//\//\\/}/g" /srv/www/htdocs/postfixadmin/config.local.php
+sed -i "s/MYHOSTNAME/${MYHOSTNAME}/g" /etc/postfixadmin/config.local.php
+sed -i "s/MYSQL_PASSWORD/${MYSQL_PASSWORD//\//\\/}/g" /etc/postfixadmin/config.local.php
 
 SETUP_PASSWORD_ENCRYPTED=$(php /usr/local/bin/setuppass.php "${SETUP_PASSWORD}");
 
 if [ $? -ne 0 ]; then
 	echo "could not generate setup password. aborting."
 else 
-	echo "\$CONF['setup_password'] = '${SETUP_PASSWORD_ENCRYPTED}';" >> /srv/www/htdocs/postfixadmin/config.local.php
+	echo "\$CONF['setup_password'] = '${SETUP_PASSWORD_ENCRYPTED}';" >> /etc/postfixadmin/config.local.php
 fi
 
 /usr/sbin/start_apache2 -DFOREGROUND -k start
