abd4b375a25b8e5cc66a300ba180a801fc1aaf0b
Thomas Schulte <thomas.schulte@suse.com>
2018-07-18 04:55:36 +0000

    use better variables for IMAP and SMTP

--- roundcube/image/target/start.sh
+++ roundcube/image/target/start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-if [ "${MYHOSTNAME}x" == "x" ]; then
-	echo "MYHOSTNAME is missing"
+if [ "${SMTP_SERVER}x" == "x" ]; then
+	echo "SMTP_SERVER is missing"
+	exit 1
+fi
+
+if [ "${IMAP_SERVER}x" == "x" ]; then
+	echo "IMAP_SERVER is missing"
 	exit 1
 fi
 
@@ -15,8 +20,8 @@ fi
 DES_KEY="$(/usr/bin/pwgen -1cay 24)"
 
 sed -i "s/.*config.*db_dsnw.*/\$config\[\'db_dsnw\'\] = \'mysql\:\/\/app\:${MYSQL_PASSWORD//\//\\/}@roundcube-mysql\/app\'\;/" /etc/roundcubemail/config.inc.php
-sed -i "s/.*config.*smtp_server.*/\$config\[\'smtp_server\'\] = \'tls:\/\/${MYHOSTNAME//\//\\/}\'\;/" /etc/roundcubemail/config.inc.php
-sed -i "s/.*config.*default_host.*/\$config\[\'default_host\'\] = \'tls:\/\/${MYHOSTNAME//\//\\/}\'\;/" /etc/roundcubemail/config.inc.php
+sed -i "s/.*config.*smtp_server.*/\$config\[\'smtp_server\'\] = \'tls:\/\/${SMTP_SERVER//\//\\/}\'\;/" /etc/roundcubemail/config.inc.php
+sed -i "s/.*config.*default_host.*/\$config\[\'default_host\'\] = \'tls:\/\/${IMAP_SERVER//\//\\/}\'\;/" /etc/roundcubemail/config.inc.php
 sed -i "s/.*config.*des_key.*/\$config\[\'des_key\'\] = \'${DES_KEY//\//\\/}\'\;/" /etc/roundcubemail/config.inc.php
 
 until nc -z roundcube-mysql 3306
